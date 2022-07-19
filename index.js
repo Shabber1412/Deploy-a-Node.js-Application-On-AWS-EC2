d9918125d1cc540f8029cff545304d61c083bc49
Shaik Shabber <87518304+Shabber1412@users.noreply.github.com>
2022-07-19 21:37:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200,{'content-Type':'text/plain'})
-    res.end("  Hello Team!! It's working 🤗 ")
+    res.end( "<h1> Hello Team!! Deploying a Node.js Application On AWS-EC2 Instance and Here It's working Successfully..... 🤗 </h1>")
   }).listen(8080);
 
 console.log('Server is Running Successfull!!!!');
+console.log('Server is Listening on your ec2 Public IPv4 address:8080');
