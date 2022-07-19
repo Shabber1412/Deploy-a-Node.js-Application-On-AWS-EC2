689ccde42dd34d6caa98630a298eda0b34f19552
Shaik Shabber <87518304+Shabber1412@users.noreply.github.com>
2022-07-19 21:24:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const http = require('http');
 
-const port = process.env.PORT || 8080;
-
 http.createServer(function (req, res) {
   res.writeHead(200,{'content-Type':'text/plain'})
     res.end("  Hello Team!! It's working 🤗 ")
-  });
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  }).listen(8080);
 
 console.log('Server is Running Successfull!!!!');
